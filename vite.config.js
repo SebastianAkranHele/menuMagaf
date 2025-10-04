9dd3956ed79f6fe6fdee6afd6a7e893e6c0a72f5
SebastianAkranHele <akranhele.17@gmail.com>
2025-10-04 14:05:18 +0100

    Actualizar o vite

--- vite.config.js
+++ vite.config.js
@@ -2,19 +2,21 @@ import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
 
 export default defineConfig({
+    base: '/menuDigital/',  // <-- Adicione esta linha
     plugins: [
         laravel({
-            input: ['resources/css/app.css',
+            input: [
+                'resources/css/app.css',
                 'resources/css/home.css',
                 'resources/css/menu.css',
                 'resources/css/admin.css',
-                  'resources/css/homehero-index.css',
+                'resources/css/homehero-index.css',
                 'resources/js/app.js',
-            'resources/js/home.js',
-            'resources/js/menu.js',
-            'resources/js/admin.js',
-    'resources/js/homehero-index.js'
-        ],
+                'resources/js/home.js',
+                'resources/js/menu.js',
+                'resources/js/admin.js',
+                'resources/js/homehero-index.js'
+            ],
             refresh: true,
         }),
     ],
